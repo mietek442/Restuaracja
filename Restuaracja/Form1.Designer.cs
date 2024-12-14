7effabffe258485ce891985ba9a3094047e56584
mietek442 <96135826+mietek442@users.noreply.github.com>
2024-12-14 19:57:07 +0100

    sraka

--- Restuaracja/Form1.Designer.cs
+++ Restuaracja/Form1.Designer.cs
@@ -30,16 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             flowLayoutPanel1 = new FlowLayoutPanel();
+            button5 = new Button();
             button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             panel1 = new Panel();
             button2 = new Button();
+            button6 = new Button();
+            button7 = new Button();
             flowLayoutPanel1.SuspendLayout();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
+            flowLayoutPanel1.Controls.Add(button5);
             flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Controls.Add(button4);
             flowLayoutPanel1.Location = new Point(372, 169);
             flowLayoutPanel1.Margin = new Padding(4);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -47,21 +55,74 @@
             flowLayoutPanel1.TabIndex = 0;
             flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
             // 
+            // button5
+            // 
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatAppearance.MouseOverBackColor = Color.FromArgb(82, 82, 82);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Impact", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.DimGray;
+            button5.Location = new Point(0, 0);
+            button5.Margin = new Padding(0);
+            button5.Name = "button5";
+            button5.Size = new Size(377, 81);
+            button5.TabIndex = 1;
+            button5.Text = "Stoliki";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
             // button1
             // 
-            button1.Location = new Point(4, 4);
-            button1.Margin = new Padding(4);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(82, 82, 82);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Impact", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.DimGray;
+            button1.Location = new Point(0, 81);
+            button1.Margin = new Padding(0);
             button1.Name = "button1";
-            button1.Size = new Size(465, 91);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Size = new Size(377, 81);
+            button1.TabIndex = 2;
+            button1.Text = "Zaapotrzenie";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(82, 82, 82);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Impact", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.DimGray;
+            button3.Location = new Point(0, 162);
+            button3.Margin = new Padding(0);
+            button3.Name = "button3";
+            button3.Size = new Size(377, 81);
+            button3.TabIndex = 3;
+            button3.Text = "Zasrana Resteuracja";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseOverBackColor = Color.FromArgb(82, 82, 82);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Impact", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.DimGray;
+            button4.Location = new Point(0, 243);
+            button4.Margin = new Padding(0);
+            button4.Name = "button4";
+            button4.Size = new Size(377, 81);
+            button4.TabIndex = 4;
+            button4.Text = "Jesteś Pedałem";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // panel1
             // 
             panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panel1.Controls.Add(button7);
             panel1.Controls.Add(button2);
+            panel1.Controls.Add(button6);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Margin = new Padding(0);
@@ -71,17 +132,47 @@
             // 
             // button2
             // 
+            button2.AccessibleRole = AccessibleRole.ButtonMenu;
             button2.FlatAppearance.BorderSize = 0;
             button2.FlatAppearance.MouseOverBackColor = Color.Red;
             button2.FlatStyle = FlatStyle.Flat;
             button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.Location = new Point(1003, 9);
+            button2.Location = new Point(993, 9);
             button2.Margin = new Padding(0);
             button2.Name = "button2";
             button2.Size = new Size(98, 81);
             button2.TabIndex = 0;
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
+            // 
+            // button6
+            // 
+            button6.AccessibleRole = AccessibleRole.ButtonMenu;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatAppearance.MouseOverBackColor = Color.Red;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.Location = new Point(900, 9);
+            button6.Margin = new Padding(0);
+            button6.Name = "button6";
+            button6.Size = new Size(93, 81);
+            button6.TabIndex = 2;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.AccessibleRole = AccessibleRole.ButtonMenu;
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatAppearance.MouseOverBackColor = Color.Red;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.Location = new Point(789, 0);
+            button7.Margin = new Padding(0);
+            button7.Name = "button7";
+            button7.Size = new Size(93, 81);
+            button7.TabIndex = 3;
+            button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -107,8 +198,13 @@
         #endregion
 
         private FlowLayoutPanel flowLayoutPanel1;
-        private Button button1;
         private Panel panel1;
         private Button button2;
+        private Button button5;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button6;
+        private Button button7;
     }
 }
--- Restuaracja/Form1.cs
+++ Restuaracja/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace Restuaracja
 {
     public partial class Form1 : Form
@@ -14,15 +18,35 @@ namespace Restuaracja
 
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
-
+            // Usuniêcie niepotrzebnego wyjœcia z aplikacji podczas malowania
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            // Obs³uga dla "Zaopatrzenie"
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Obs³uga dla "Stoliki"
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Obs³uga dla "Zasrana Resteuracja"
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Obs³uga dla "Jesteœ Peda³em"
+        }
+
+        private void button6_Click(object sender, EventArgs e)
         {
 
         }
