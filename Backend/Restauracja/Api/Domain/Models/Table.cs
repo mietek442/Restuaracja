f7016d7b1b8c93d74ca1e5037a3e4cc54b3c7f05
mietek442 <96135826+mietek442@users.noreply.github.com>
2025-01-03 19:06:49 +0100

    To ten jest CreateTable, w sumie to chat zrobił w 5 minut to, ale mam dosyć tego .Neta i idę chlać wróce wieczorem i jak się nie zacheleje na śmierć to coś zrobie xd

---
+++ Backend/Restauracja/Api/Domain/Models/Table.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Domain.Models
+{
+    public class Table
+    {
+        Guid Id { get; set; }
+        public int SeatNumber { get; set; }
+        // jak coś to jest taki opis że stolik przy Oknie, Stolik dla Palacych, Stolik dla Meneli itd.
+        public string Description { get; set; }
+        public bool IsReservate { get; set; }
+    }
+}
